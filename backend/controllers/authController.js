e636324aed7b5157b35340f2b4eeb600dc5f2a02
tr-prajith <prajithappu1357@gmail.com>
2026-06-13 10:46:16 +0530

    fixed cookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -31,8 +31,8 @@ const register = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "lax",
+            secure: true,        
+            sameSite: "none",    
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
